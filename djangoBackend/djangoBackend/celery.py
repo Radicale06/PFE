633294a60518aaeaa94fe1a27bce4ca00460e6ad
Radicale06 <chaabaneoussama@fss.u-sfax.tn>
2025-05-27 17:14:42 +0100

    Clean up tracked files to match .gitignore

--- djangoBackend/djangoBackend/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoBackend.settings')
-
-app = Celery('celery_app')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
